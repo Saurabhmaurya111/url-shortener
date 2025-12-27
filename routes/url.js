6f547ac9d21dc8a7b61ec00ccd337fc24d226767
Saurabh Maurya <saurabhmaurya2050@gmail.com>
2025-12-27 20:11:55 +0530

    all done

--- routes/url.js
+++ routes/url.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {handleGenerateNewShortUrl , handleAnalytics} = require("../controllers/url");
 const router = express.Router();
 
+
 router.post("/" , handleGenerateNewShortUrl);
 router.get("/analytics/:shortId" , handleAnalytics )
 module.exports = router;
