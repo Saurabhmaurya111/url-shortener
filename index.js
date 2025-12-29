b5e8875c1bfd7ec31d6ed66ce69f7a96a09f5f4f
Saurabh Maurya <saurabhmaurya2050@gmail.com>
2025-12-29 18:45:30 +0530

    now is time for jwt in the next commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const URL = require("./models/url");
 const urlRoute = require("./routes/url");
-const {connectDB} = require("./connect")
+const path = require("path");
+const {connectDB} = require("./connect");
 
 const app = express();
 const PORT = 8001;
@@ -13,8 +14,18 @@ connectDB("mongodb://127.0.0.1:27017/short-url").then(() => {
 app.use(express.json());
 
 app.use("/url" , urlRoute)
+app.set("view engine" , "ejs")
+app.set("views" , path.resolve("./views") )
+
+app.get("/test" , async (req, res) => {
+    const allUrls = await URL.find({})
+    res.render("home", {
+
+        urls : allUrls
+    });
+});
  
-app.get("/:shortId" , async(req, res)=>{
+app.get("/api/:shortId" , async(req, res)=>{
      const shortId = req.params.shortId;
      const entry = await URL.findOneAndUpdate(
         {shortId},
